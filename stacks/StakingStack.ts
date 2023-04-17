f9ae3fc23d185a3b9c6c9333d909ce2fa354b6e0
Daniele Salatti <me@danielesalatti.com>
2023-04-17 09:40:16 -0700

    chore: clean up stack

--- stacks/StakingStack.ts
+++ stacks/StakingStack.ts
@@ -6,23 +6,9 @@ export function StakingStack({ stack }: StackContext) {
   const cluster = new RDS(stack, "Cluster", {
     engine: "mysql5.7",
     defaultDatabaseName: DATABASE,
-    // Migration scripts are run manually with prisma migrate
     migrations: "services/migrations",
   });
-  /*
-  const memberTable = new Table(stack, "Member", {
-    fields: {
-      first_name: "string",
-      last_name: "string",
-      wallet: "string",
-      country: "string",
-      email: "string",
-      date: "string",
-      is_active: "binary",
-    },
-    primaryIndex: { partitionKey: "wallet", sortKey: "date" },
-  });
-*/
+
   const inboundQueue = new Queue(stack, "Queue", {
     consumer: "packages/functions/src/inboundConsumer.handler",
   });
@@ -32,15 +18,11 @@ export function StakingStack({ stack }: StackContext) {
     // customDomain: "custom-domain.com",
     edge: true,
     environment: {
-      // Pass the table details to our app
-      // MEMBER_TABLE_NAME: memberTable.tableName,
       DB_HOST: cluster.clusterIdentifier,
       INBOUND_QUEUE: inboundQueue.queueUrl,
-      //INBOUND_MEMBER_TABLE_NAME: InboundMemberTable.tableName,
     },
   });
 
-  // site.attachPermissions([memberTable]);
   site.attachPermissions([cluster]);
 
   stack.addOutputs({
