777ab64c7937c542b2f9afa345321c967ce493c4
Daniele Salatti <me@danielesalatti.com>
2023-05-08 22:43:21 -0700

    feat: refactor interface in common file for lambdas

---
+++ packages/functions/src/common.ts
@@ -0,0 +1,28 @@
+export interface Database {
+  member: {
+    id?: number;
+    first_name: string;
+    last_name: string;
+    wallet: string;
+    country: string;
+    email: string;
+    amount: string;
+    created_at?: Date;
+    is_active: boolean;
+  };
+  stake_log: {
+    wallet: string;
+    total_amount: string;
+    created_at?: Date;
+  };
+  running_totals: {
+    wallet: string;
+    amount: string;
+    percentage_share: number;
+    updated_at?: Date;
+  };
+  contract_running_total: {
+    amount: string;
+    created_at?: Date;
+  };
+}
